94b02dc137eee9f3d81b7ed4fd008bd30c304707
Dani Bachini <danielle.bachini@gmail.com>
2023-02-01 15:24:04 +0100

    Flask, bcrypt, and mysql.connector properly installed and import fixed

--- galeeza/galeeza/app.py
+++ galeeza/galeeza/app.py
@@ -1,12 +1,19 @@
 # read https://www.loekvandenouweland.com/content/full-path-in-zsh-shell.html
 
-import mysql.connector
-from bcrypt import hashpw, checkpw, gensalt
-# from flask import Flask, flash, redirect, render_template, request, session
-
 # How to use bcrypt: 
 # https://blog.carsonevans.ca/2020/08/02/storing-passwords-in-flask/ 
 # https://www.npmjs.com/package/bcrypt?activeTab=readme
+
+import sys
+import mysql.connector
+from bcrypt import hashpw, checkpw, gensalt
+from flask import Flask, flash, redirect, render_template, request, session
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask"
 
 
 db = mysql.connector.connect(
@@ -17,8 +24,7 @@ db = mysql.connector.connect(
 )
 
 
-# check how difficult is django - compared to flask
 
-
-
-
+# FLASK AND BCRYPT NOT WORKING
+# https://stackoverflow.com/questions/71277049/import-could-not-be-resolved-pylancereportmissingimports
+# https://stackoverflow.com/questions/52581576/could-not-import-d-flask-app
