f510373db5a1caf600835e50d3cc99777528613a
Dani Bachini <danielle.bachini@gmail.com>
2023-03-06 11:03:46 +0100

    Preferences form working

--- galeeza/galeeza/app.py
+++ galeeza/galeeza/app.py
@@ -4,14 +4,15 @@
 # https://blog.carsonevans.ca/2020/08/02/storing-passwords-in-flask/ 
 # https://www.npmjs.com/package/bcrypt?activeTab=readme
 
-import sys
-import os
+# import sys
+# import os
+# import json
 import mysql.connector  # documentation: https://dev.mysql.com/doc/connector-python/en/connector-python-example-cursor-transaction.html 
 from bcrypt import hashpw, checkpw, gensalt
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, flash, jsonify
 from flask_session import Session
 # import pymysql
-from datetime import datetime
+# from datetime import datetime
 import re
 from galeeza.helpers import apology, login_required
 
@@ -22,14 +23,16 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-db = mysql.connector.connect(
+datab = mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="Thg489!asf",
     database="galeeza"
 )
 
-mycursor = db.cursor()
+mycursor = datab.cursor()
+
+# CHECK DB DESIGN AT https://app.sqldbm.com/MySQL/Edit/p249319/ 
 
 @app.route("/")
 @login_required
@@ -50,15 +53,16 @@ def signup():
         password = request.form["password"]
         confirmation = request.form["confirmation"]
 
-        # check on the db table the row where email equals the email provided by the user
+        # check on the table if there's a row where email equals the email provided by the user
         select_query = ("SELECT * FROM users WHERE email=%s")
         user_email = (email,)
         mycursor.execute(select_query, user_email)
         result = mycursor.fetchone()
 
-        # check some constraints about the input
         if result:
             return apology("There's already an account registered with this email")
+        
+        # check some constraints about the inputs
         if first_name == "" or last_name == "" or email == "" or password == "" or confirmation == "":
             return apology("All fields are required")
         if password != confirmation:
@@ -70,7 +74,7 @@ def signup():
         if not re.search("[A-Z]", password):
             return apology("Password must contain at least 1 uppercase letter")
 
-        # if everything about the input is good, insert the user to the table
+        # if everything about the inputs is good, insert the user to the table
         else: 
             # hash password with salt
             hash = hashpw(password.encode('utf-8'), gensalt())
@@ -79,7 +83,7 @@ def signup():
             insert_query = ("INSERT INTO users (first_name, last_name, email, hash) VALUES(%s, %s, %s, %s)")
             data_user = (first_name, last_name, email, hash)
             mycursor.execute(insert_query, data_user)
-            db.commit()
+            datab.commit()
 
             # get the user id from the table users
             select_query = ("SELECT * FROM users WHERE email=%s")
@@ -93,8 +97,6 @@ def signup():
 
     else:
         return render_template("signup.html")
-
-    # ------- FIX BROKEN IMAGE SIGN UP ------------ 
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -113,7 +115,7 @@ def login():
         if email == "" or password == "":
             return apology("All fields are required")
 
-        # check on the db table the row where email equals the email provided by the user
+        # check on the datab table the row where email equals the email provided by the user
         select_query = ("SELECT * FROM users WHERE email=%s")
         user_email = (email,)
         mycursor.execute(select_query, user_email)
@@ -145,7 +147,7 @@ def login():
 
 
 
-@app.route("/logout")
+@app.route("/logout", methods=["GET", "POST"])
 def logout():
     """Log user out"""
 
@@ -156,17 +158,115 @@ def logout():
     return redirect("/")
 
 
+@app.route("/preferences", methods=["GET", "POST"])
+@login_required
+def preferences():
+    """Take the user to fill in their preferences"""
+
+    if request.method == "POST":
+        selected_options = request.get_json()
+        print(selected_options)
+
+        # check the id of the user preferences in the user_preferences table
+        id_query = ("SELECT id FROM user_preferences WHERE id_user=%s")
+        session_user = (session["user_id"],)
+        mycursor.execute(id_query, session_user)
+        id_pref, = mycursor.fetchone()  # the comma is to use tuple unpacking to extract the value 
+
+        for i in selected_options: 
+            # check on the datab table the row where category equals the category selected by the user
+            select_query = ("SELECT category FROM types WHERE category=%s")
+            category = (i,)
+            mycursor.execute(select_query, category)
+            result = mycursor.fetchone()
+
+            # add all the preferences the user selected to the table chosen_preferences
+            if result:
+                # get the id of the selected category in the types table
+                types_query = ("SELECT id FROM types WHERE category=%s")
+                category_type = (i,)
+                mycursor.execute(types_query, category_type)
+                category_id, = mycursor.fetchone()  # the comma is to use tuple unpacking to extract the value 
+
+                # get the id of the category 
+                find_query = ("SELECT id_user_pref FROM chosen_preferences WHERE id_type=%s")
+                find_type = (category_id,)
+                mycursor.execute(find_query, find_type)
+                find_result = mycursor.fetchone()
+
+                # if the category does not exist in the user's chosen_preferences table, add it
+                if not(find_result): 
+                    # add the user selected preference to the chosen_preferences table
+                    insert_pref = ("INSERT INTO chosen_preferences (id_type, id_user_pref, bool_value) VALUES (%s, %s, %s)")
+                    values_pref = (category_id, id_pref, 1)
+                    mycursor.execute(insert_pref, values_pref)
+                    datab.commit()
+
+            # add daily cost in the table user_preferences
+            else:
+                check_query = ("SELECT id_user FROM user_preferences WHERE id_user=%s")
+                id_user = (session["user_id"],)
+                mycursor.execute(check_query, id_user)
+                result_id = mycursor.fetchone()
+
+                # if user had previously selected a value, update it for the new value
+                if result_id:
+                    update_query = ("UPDATE user_preferences SET cost_day=%s WHERE id_user=%s")
+                    new_cost = (i,session["user_id"])
+                    mycursor.execute(update_query, new_cost)
+                    datab.commit()
+
+                # if first time user is selecting a value, add it to the table
+                else:
+                    insert_query = ("INSERT INTO user_preferences (cost_day, id_user) VALUES (%s, %s)")
+                    values_user = (i, session["user_id"])
+                    mycursor.execute(insert_query, values_user)
+                    datab.commit()
+
+        return redirect("plan.html")
+
+    else:
+        return render_template("preferences.html")
 
 
-@app.route("/plan")
+
+@app.route("/plan", methods=["GET", "POST"])
 @login_required
 def plan():
     """Take the user to plan the trip"""
-    return render_template("plan.html")
+
+    if request.method == "POST":
+        return "TODO"
+
+    else:
+        # PSEUDOCODE
+        # user clicks on plan
+        # check on the datab if the preferences with that id exist (aka row = 1)
+        # if it doesn't exist - redirect user to form
+        # if it does exist - redirect user to plan
 
 
 
-@app.route("/profile")
+
+        # check on the datab table if there is at least one row with the session user_id of the user logged in
+        select_query = ("SELECT * FROM user_preferences WHERE id_user=%s") # <<<<---!!!!! CHANGE THIS it doesn't guarantee the user answered all the questions !!!!
+        id_user = (session["user_id"],)
+        mycursor.execute(select_query, id_user)
+        result = mycursor.fetchone()
+
+        # if there's no row with the session user_id logged in
+        if not result:
+            return redirect("/preferences")
+
+        # if the user already filled in their preferences
+        return render_template("plan.html")
+
+
+
+
+
+
+@app.route("/profile", methods=["GET", "POST"])
 @login_required
 def profile():
     """Display the user's profile"""
---
+++ galeeza/galeeza/static/js/script.js
@@ -0,0 +1,163 @@
+// MULTIPLE STEPS - PREFERENCES FORM
+// code adapted from https://designmodo.com/bootstrap-5-form/ 
+
+let step = document.getElementsByClassName('step');
+let prevBtn = document.getElementById('prev-btn');
+let nextBtn = document.getElementById('next-btn');
+let submitBtn = document.getElementById('submit-btn');
+let form = document.getElementsByTagName('form')[0];
+let preloader = document.getElementById('preloader-wrapper');
+let bodyElement = document.querySelector('body');
+let succcessDiv = document.getElementById('success');
+
+form.onsubmit = () => {
+    return false
+}
+
+let current_step = 0;
+let stepCount = 3;
+
+step[current_step].classList.add('d-block');
+if (current_step == 0) {
+    prevBtn.classList.add('d-none');
+    submitBtn.classList.add('d-none');
+    nextBtn.classList.add('d-inline-block');
+}
+
+const progress = (value) => {
+    document.getElementsByClassName('progress-bar')[0].style.width = `${value}%`;
+}
+
+nextBtn.addEventListener('click', () => {
+    current_step++;
+    let previous_step = current_step - 1;
+    if ((current_step > 0) && (current_step <= stepCount)) {
+        prevBtn.classList.remove('d-none');
+        prevBtn.classList.add('d-inline-block');
+        step[current_step].classList.remove('d-none');
+        step[current_step].classList.add('d-block');
+        step[previous_step].classList.remove('d-block');
+        step[previous_step].classList.add('d-none');
+        if (current_step == stepCount) {
+            submitBtn.classList.remove('d-none');
+            submitBtn.classList.add('d-inline-block');
+            nextBtn.classList.remove('d-inline-block');
+            nextBtn.classList.add('d-none');
+        }
+    } 
+    else {
+        if (current_step > stepCount) {
+            form.onsubmit = () => {
+                return true
+            }
+        }
+    }
+    progress((100 / stepCount) * current_step);
+});
+ 
+prevBtn.addEventListener('click', () => {
+    if (current_step > 0) {
+        current_step--;
+        let previous_step = current_step + 1;
+        prevBtn.classList.add('d-none');
+        prevBtn.classList.add('d-inline-block');
+        step[current_step].classList.remove('d-none');
+        step[current_step].classList.add('d-block')
+        step[previous_step].classList.remove('d-block');
+        step[previous_step].classList.add('d-none');
+        if (current_step < stepCount) {
+            submitBtn.classList.remove('d-inline-block');
+            submitBtn.classList.add('d-none');
+            nextBtn.classList.remove('d-none');
+            nextBtn.classList.add('d-inline-block');
+            prevBtn.classList.remove('d-none');
+            prevBtn.classList.add('d-inline-block');
+        }
+    }
+ 
+    if (current_step == 0) {
+        prevBtn.classList.remove('d-inline-block');
+        prevBtn.classList.add('d-none');
+    }
+    progress((100 / stepCount) * current_step);
+});
+
+// function getting the values selected in the checkboxes
+function store_checkbox_value() {
+    let selected_options = [];
+    let checkboxes = document.querySelectorAll("input[type='checkbox']:checked");
+    for (let i = 0; i < checkboxes.length; i += 1) {
+        selected_options.push(checkboxes[i].value)}
+    
+    let amount_cash = document.getElementById('cash_day')   // get the value in the range
+    selected_options.push(amount_cash.value) 
+
+    // const s = JSON.stringify(selected_options);  // s is a list 
+    console.log(selected_options);  // -----------!!!!!!!!! can be removed later - it prints the values in the console on the browser and terminal  !!!!!!!!!!!-----------------
+    // window.alert(selected_options)  // -----------!!!!!!!!! can be removed later - alert window on the browser !!!!!!!!!!!-----------------------
+
+    // sends an AJAX request to the server with the values selected in the checkboxes
+    $.ajax({
+        type: "POST",
+        url: "/preferences",
+        data: JSON.stringify(selected_options),
+        contentType: "application/json",
+        dataType: "json"
+    })
+};
+
+// function replacing the URL and the user can't go back the previous page (leaving the preferences form)
+function replace_function(){
+    window.location.replace("plan");
+}
+ 
+
+ 
+
+// submitBtn.addEventListener('click', () => {
+//     preloader.classList.add('d-block');
+ 
+//     const timer = ms => new Promise(res => setTimeout(res, ms));
+ 
+//     timer(3000)
+//         .then(() => {
+//             bodyElement.classList.add('loaded');
+//         }).then(() => {
+//             step[stepCount].classList.remove('d-block');
+//             step[stepCount].classList.add('d-none');
+//             prevBtn.classList.remove('d-inline-block');
+//             prevBtn.classList.add('d-none');
+//             submitBtn.classList.remove('d-inline-block');
+//             submitBtn.classList.add('d-none');
+//             succcessDiv.classList.remove('d-none');
+//             succcessDiv.classList.add('d-block');
+//         })
+ 
+// });
+
+
+
+// RANGE
+// code from https://css-tricks.com/value-bubbles-for-range-inputs/
+
+const allRanges = document.querySelectorAll(".range-wrap");
+allRanges.forEach(wrap => {
+  const range = wrap.querySelector(".range");
+  const bubble = wrap.querySelector(".bubble");
+
+  range.addEventListener("input", () => {
+    setBubble(range, bubble);
+  });
+  setBubble(range, bubble);
+});
+
+function setBubble(range, bubble) {
+  const val = range.value;
+  const min = range.min ? range.min : 0;
+  const max = range.max ? range.max : 100;
+  const newVal = Number(((val - min) * 100) / (max - min));
+  bubble.innerHTML = ("$ " + val);
+
+  bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
+}
+
